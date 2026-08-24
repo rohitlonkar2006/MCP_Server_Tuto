e94d9f68a7b28bcf8344359db79f41b24aba30e0
Rohit Lonkar <lonkarravi00@gmail.com>
2026-08-24 21:17:20 +0530

    done with today

--- CH-2_HTTP_MCP/1_http_mcp.py
+++ CH-2_HTTP_MCP/1_http_mcp.py
@@ -1,16 +1,17 @@
 from fastmcp import FastMCP
+import asyncio
 
 mcp = FastMCP()
 
 @mcp.tool()
-def fetch():
+async def fetch():
     """Use This Tool To Fetch Data from a source"""
     return {"data":"Hello,MCP!"}
 
 @mcp.tool()
-def process(path:str):
+async def process(path:str):
     """Use This Tool To Process Data from a source"""
     return {"processed_data":"Data Has Been Processed at path: "+path}
 
 if __name__ == "__main__":
-    mcp.run(transport = "streamable-http")
+    mcp.run(transport = "streamable-http",host = "0.0.0.0",port = 8050)
