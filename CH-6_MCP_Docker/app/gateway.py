cc3c105e7537ca4e5169c7595d166e326af35f23
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-09 16:57:28 +0530

    just docker things🤞

---
+++ CH-6_MCP_Docker/app/gateway.py
@@ -0,0 +1,34 @@
+from fastmcp import FastMCP
+from fastmcp.server import create_proxy
+
+mcp = FastMCP("MCP Gateway")
+
+@mcp.tool()
+async def fetch_http():
+    """Use This Tool To Fetch Data from a source"""
+    return {"data": "Hello, MCP!"}
+
+@mcp.tool()
+async def process_http(path: str):
+    """Use This Tool To Process Data from a source"""
+    return {"processed_data": "Data Has Been Processed at path: " + path}
+
+
+mcp.mount(
+    create_proxy({
+        "mcpServers": {
+            "ddg_mcp": {
+                "command": "uvx",
+                "args": ["duckduckgo-mcp-server"]
+            }
+        }
+    }),
+    namespace="ddg_mcp"
+)
+
+if __name__ == "__main__":
+    mcp.run(
+        transport="streamable-http",
+        host="0.0.0.0",
+        port=8050
+    )
